77021d664dbc92e2466dd7e20721b0921e9633d0
IO <20157367@tafe.wa.edu.au>
2025-09-03 11:03:41 +0800

    Update pythonFile.py
    A set of heavy-handed changes that shouldn't make a difference

--- pythonFile.py
+++ pythonFile.py
@@ -1,6 +1,12 @@
-import random as rd
+import random
 
-random_numbers = [rd.randint(1,100) for i in range(20)]
+completely_random_numbers = [random.randint(1,100) for i in range(20)]
 
-print(" 20 random numbers: ")
-print(random_numbers)
+# Who knows, might need these 3 variables one day
+
+num1 = 1
+num2 = 2
+num3 = 3
+
+print(" These are 20 random numbers that have been generated just now: ")
+print(completely_random_numbers)
